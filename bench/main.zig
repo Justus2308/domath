cabdc3826a7ac06116f99edecb1d921a1208f4f8
Justus Klausecker <justus@klausecker.de>
2025-05-10 14:20:58 +0200

    bench: bug fixes

--- bench/main.zig
+++ bench/main.zig
@@ -23,14 +23,13 @@ fn benchDomathBasic(allocator: std.mem.Allocator) void {
 
     var list_in = List.empty;
     defer list_in.deinit(allocator);
+    list_in.setCapacity(allocator, basic_vec_count) catch @panic("OOM");
+    list_in.len = basic_vec_count;
 
     var list_out = List.empty;
     defer list_out.deinit(allocator);
-
-    list_in.setCapacity(allocator, basic_vec_count) catch @panic("OOM");
-    list_in.len = List.capacityInBytes(basic_vec_count);
     list_out.setCapacity(allocator, basic_vec_count) catch @panic("OOM");
-    list_out.len = List.capacityInBytes(basic_vec_count);
+    list_out.len = basic_vec_count;
 
     var rand = std.Random.DefaultPrng.init(0);
     rand.fill(list_in.bytes[0..List.capacityInBytes(basic_vec_count)]);
@@ -38,9 +37,8 @@ fn benchDomathBasic(allocator: std.mem.Allocator) void {
     const slice_in = list_in.slice();
     const slice_out = list_out.slice();
 
-    const batch_count = @divExact(basic_vec_count, v4.vectors_per_op);
-    for (0..batch_count) |i| {
-        const offset = (i * v4.vectors_per_op);
+    var offset: usize = 0;
+    while (offset < basic_vec_count) : (offset += v4.vectors_per_op) {
         const in = v4.fromMultiArrayList(slice_in, .{ .x, .y, .z, .w }, offset);
         const out = v4.fromMultiArrayList(slice_out, .{ .x, .y, .z, .w }, offset);
         v4.normalize(&in, &out);
@@ -52,11 +50,10 @@ fn benchZalgebraBasic(allocator: std.mem.Allocator) void {
 
     var list_in = List.initCapacity(allocator, basic_vec_count) catch @panic("OOM");
     defer list_in.deinit(allocator);
+    list_in.expandToCapacity();
 
     var list_out = List.initCapacity(allocator, basic_vec_count) catch @panic("OOM");
     defer list_out.deinit(allocator);
-
-    list_in.expandToCapacity();
     list_out.expandToCapacity();
 
     var rand = std.Random.DefaultPrng.init(0);
